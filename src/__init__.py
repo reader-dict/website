c5abcfa342c8e49c02f37751cfe3004a4748a3c3
Mickaël Schoentgen <contact@tiger-222.fr>
2025-09-18 21:11:52 +0200

    feat: make buttons "responsive" to users hover

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ You can always get the latest version at:
     https://github.com/reader-dict/website
 """
 
-__version__ = "60"
+__version__ = "61"
 __author__ = "Mickaël Schoentgen"
 __copyright__ = f"""
 Copyright (c) 2020-2025, {__author__}
